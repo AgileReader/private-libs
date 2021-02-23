42faae2b77dc6f41ec6a38200eb6242dc7e430d6
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-02-23 18:13:30 +0100

    refactor: index export file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const aolib = require('./aolib');
+const datelib = require('./datelib');
+const filelib = require('./filelib');
+const strlib = require('./strlib');
+
+module.exports = {
+  aolib,
+  datelib,
+  filelib,
+  strlib,
+};
