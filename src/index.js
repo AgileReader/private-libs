65928179c336d5e968cf6d78b205b5854b4f990f
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-02-24 09:59:10 +0100

    chore: export brandlib

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const aolib = require('./aolib');
 const datelib = require('./datelib');
 const filelib = require('./filelib');
 const strlib = require('./strlib');
+const brandlib = require('./brandlib');
 
 module.exports = {
   aolib,
   datelib,
   filelib,
   strlib,
+  brandlib,
 };
